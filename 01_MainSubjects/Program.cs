c25e57849d9d9fccfba593e24e95cb514d541010
Muhammet Ali Görey <muhammetaligorey@gmail.com>
2024-12-03 22:48:27 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya"); // imleç aşağıya iner
+
+            Console.Write("Selam");
+
+            Console.Read();
+        }
+    }
+}
